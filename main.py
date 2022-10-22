9e2beb049d2c58388278c6b671caa4daacc0e1fd
Yuriy Kim <yura.kim-KIM@yandex.ru>
2022-10-22 17:33:51 +0300

    metric bug fix

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import pandas as pd
 import utils
 from catboost import CatBoostClassifier
+import numpy as np
+from tqdm import tqdm
 
 
 if __name__ == "__main__":
     data = pd.read_csv('data/train.csv')
     database, query = utils.data_test_split(data)
     request = database[['name_2']].copy()
-    company_id = int(input())
-    company_name = query[company_id]
-    true_val = [0]*len(database)
-    ind = database[database['name_1'] == company_name].index[0]
-    true_val[ind] += 1
-    request = utils.prep_data(request, company_name)
-    feat_cols = [
-        'name_1', 'name_2', 'len_intersection', 'len_str_1', 'len_words_1',
-        'len_str_2', 'len_words_2','fuzz_ratio', 'jaccard']
+    
     model = CatBoostClassifier() 
     model.load_model('catboost_model.bin')
-    pred_proba = model.predict_proba(request[feat_cols])[:, 1]
-    utils.get_metrics(pred_proba, true_val)
+    feat_cols = [
+            'name_1', 'name_2', 'len_intersection', 'len_str_1', 'len_words_1',
+            'len_str_2', 'len_words_2','fuzz_ratio', 'jaccard'
+        ]
+    
+    pred_probas = np.zeros(shape=(len(query), len(database)))
+    true_vals = np.zeros(shape=(len(query), 1))
+    for company_id in tqdm(range(len(query))):
+        company_name = query[company_id]
+        true_val = database[database['name_1'] == company_name].index[0]
+        request = utils.prep_data(request, company_name)
+        pred_proba = model.predict_proba(request[feat_cols])[:, 1]
+        true_vals[company_id] = true_val
+        pred_probas[company_id] = pred_proba
+    labels = range(len(database))
+    utils.get_metrics(pred_probas, true_vals, labels)
--- utils.py
+++ utils.py
@@ -1,26 +1,20 @@
 from sklearn.model_selection import train_test_split
 from fuzzywuzzy import fuzz
-# from cdifflib import CSequenceMatcher
-import tensorflow as tf
+from sklearn.metrics import top_k_accuracy_score
 
 
 def data_test_split(data):
     _, database = train_test_split(
         data, test_size = 0.2,
-        stratify=data['is_duplicate'], random_state=53)
+        stratify=data['is_duplicate'], random_state=53
+    )
     _, query = train_test_split(
-        database, test_size = 0.5,
-        stratify=database['is_duplicate'], random_state=53)
+        database, test_size = 0.04,
+        stratify=database['is_duplicate'], random_state=53
+    )
     query = query[query['is_duplicate']==1]['name_1'].values.tolist()
     return database.reset_index(), query
 
-# def standardize_text(df, text_field):
-#     df[text_field] = df[text_field].str.lower()
-#     df[text_field] = df[text_field].str.replace(r"[0-9().,!?@\'\`\"\_\n-_]", " ")
-#     return df
-
-# def seq_match(str1, str2):
-#     return CSequenceMatcher(None, str1, str2).ratio()
 
 def prep_data(data, company_name):
     data['name_1'] = company_name.lower().replace(r"[0-9().,!?@\'\`\"\_\n-_]", " ")
@@ -32,11 +26,11 @@ def prep_data(data, company_name):
     data['len_intersection'] = data.apply(lambda x: len(set(x['name_1'].split(' ')) & set(x['name_2'].split(' '))), axis = 1)
     data['fuzz_ratio'] = data.apply(lambda x: fuzz.ratio(x['name_1'], x['name_2']), axis = 1)
     data['jaccard'] = data.apply(lambda x: x['len_intersection']/(x['len_str_1'] + x['len_str_2'] - x['len_intersection']), axis=1)
-    # data['seq_match'] = data.apply(lambda x: seq_match(x['name_1'], x['name_2']), axis=1)
     return data
 
-def get_metrics(preds, true, ks = [1, 3, 5, 10, 20, 100, 1000]):
+
+def get_metrics(preds, true, labels, ks = [5, 10, 20, 50]):
+    print('database size - ', len(preds[0]))
+    print('test size - ', len(preds))
     for k in ks:
-        acc_k = tf.keras.metrics.TopKCategoricalAccuracy(k=1)
-        acc_k.update_state([true], [preds])
-        print('k:', acc_k.result().numpy())
+        print(f'top {k} accuracy score:', top_k_accuracy_score(true, preds, k=k, labels=labels))
