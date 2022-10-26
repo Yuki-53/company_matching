e6e78052fa5dd929a5da8912426e05c6ee07f756
Yuriy Kim <yura.kim-KIM@yandex.ru>
2022-10-26 21:17:41 +0300

    add docs

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 import pandas as pd
 import utils
-from catboost import CatBoostClassifier
 import numpy as np
+
+from catboost import CatBoostClassifier
 from tqdm import tqdm
 
+def get_pred(database, company_name, model, feats):
+    request = utils.prep_data(database[['name_2']], company_name)
+    pred_proba = model.predict_proba(request[feats])[:, 1]
+    return pred_proba
 
-if __name__ == "__main__":
+
+def main():
     data = pd.read_csv('data/train.csv')
     database, query = utils.data_test_split(data)
-    request = database[['name_2']].copy()
     
     model = CatBoostClassifier() 
     model.load_model('catboost_model.bin')
     feat_cols = [
-            'name_1', 'name_2', 'len_intersection', 'len_str_1', 'len_words_1',
-            'len_str_2', 'len_words_2','fuzz_ratio', 'jaccard'
-        ]
+        'name_1', 'name_2', 'len_intersection', 'len_str_1', 'len_words_1',
+        'len_str_2', 'len_words_2','fuzz_ratio', 'jaccard']
     
     pred_probas = np.zeros(shape=(len(query), len(database)))
     true_vals = np.zeros(shape=(len(query), 1))
     for company_id in tqdm(range(len(query))):
         company_name = query[company_id]
         true_val = database[database['name_1'] == company_name].index[0]
-        request = utils.prep_data(request, company_name)
-        pred_proba = model.predict_proba(request[feat_cols])[:, 1]
+        pred_proba = get_pred(database, company_name, model, feat_cols)
         true_vals[company_id] = true_val
         pred_probas[company_id] = pred_proba
     labels = range(len(database))
     utils.get_metrics(pred_probas, true_vals, labels)
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -6,12 +6,11 @@ from sklearn.metrics import top_k_accuracy_score
 def data_test_split(data):
     _, database = train_test_split(
         data, test_size = 0.2,
-        stratify=data['is_duplicate'], random_state=53
-    )
+        stratify=data['is_duplicate'], random_state=53)
+    database = database[:10000]  # limit db only for demo
     _, query = train_test_split(
-        database, test_size = 0.04,
-        stratify=database['is_duplicate'], random_state=53
-    )
+        database, test_size = 0.5,  
+        stratify=database['is_duplicate'], random_state=53)
     query = query[query['is_duplicate']==1]['name_1'].values.tolist()
     return database.reset_index(), query
 
@@ -22,15 +21,23 @@ def prep_data(data, company_name):
     data['len_words_1'] =  len(company_name.split())
     data['name_2'] = data['name_2'].str.lower().replace(r"[0-9().,!?@\'\`\"\_\n-_]", " ")
     data['len_str_2'] =  data['name_2'].str.len()
-    data['len_words_2'] =  data['name_2'].str.split().str.len()
-    data['len_intersection'] = data.apply(lambda x: len(set(x['name_1'].split(' ')) & set(x['name_2'].split(' '))), axis = 1)
-    data['fuzz_ratio'] = data.apply(lambda x: fuzz.ratio(x['name_1'], x['name_2']), axis = 1)
-    data['jaccard'] = data.apply(lambda x: x['len_intersection']/(x['len_str_1'] + x['len_str_2'] - x['len_intersection']), axis=1)
+    data['len_words_2'] =  data['name_2'].str.split().len()
+    data['len_intersection'] = data.apply(
+        lambda x: len(set(x['name_1'].split(' ')) & set(x['name_2'].split(' '))),
+        axis = 1)
+    data['fuzz_ratio'] = data.apply(
+        lambda x: fuzz.ratio(x['name_1'], x['name_2']), 
+        axis = 1)
+    data['jaccard'] = data.apply(
+        lambda x: x['len_intersection']/(x['len_str_1'] + x['len_str_2'] - x['len_intersection']),
+        axis = 1)
     return data
 
 
 def get_metrics(preds, true, labels, ks = [5, 10, 20, 50]):
-    print('database size - ', len(preds[0]))
-    print('test size - ', len(preds))
+    print('database size -', len(preds[0]))
+    print('test size -', len(preds))
     for k in ks:
-        print(f'top {k} accuracy score:', top_k_accuracy_score(true, preds, k=k, labels=labels))
+        print(
+            f'top {k} accuracy score:', 
+            top_k_accuracy_score(true, preds, k=k, labels=labels))
